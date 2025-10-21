6ea15596ead157e0a8c7cf81fa373f3d431f50ad
Soma Thanush <somathanush10@gmail.com>
2025-10-21 21:13:41 +0530

    Initial upload of Parking Plot Picker project files

---
+++ ParkingSpacePicker.py
@@ -0,0 +1,32 @@
+import cv2
+import pickle
+
+
+width,height=107,48
+try:
+    with open('CarParkPos', 'rb') as f:
+        poslist1=pickle.load(f)
+except:
+    poslist1=[]
+
+def mouseClick(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        poslist1.append((x,y))
+    if event==cv2.EVENT_RBUTTONDOWN:
+        for i,pos in enumerate(poslist1):
+            x1,y1=pos
+            if x1<x<x1+width and y1<y<y1+height:
+                poslist1.pop(i)
+
+    with open('CarParkPos','wb') as f:
+        pickle.dump(poslist1,f)
+
+
+while True:
+    img = cv2.imread(r'C:\Users\thanu\Downloads\carParkImg.png')
+    for pos in poslist1:
+        cv2.rectangle(img,pos,(pos[0]+width,pos[1]+height), (255, 0, 255), 2)
+    cv2.imshow("image", img)
+    cv2.setMouseCallback("image",mouseClick)
+    cv2.waitKey(1)
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Parking-Plot-Availability-Checker-
+My project is Parking PLot Availability Checker ,it helps the drivers to know the available parking plot near them so that they can access the plot easily.It shows the CCTV video of the parking plots and the CCTV video is encrypted
